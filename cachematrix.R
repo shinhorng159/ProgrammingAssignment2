8182a55fee709494a3b86a2e630c7de5bc7be008
shinhorng159 <shinhorng159@gmail.com>
2015-02-20 23:05:39 +0800

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,31 +3,6 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) { 
-         # sets x equal to an empty matrix 
-         I <- NULL 
-         # Set the inverse equal to NULL 
-         
-         set <- function(y){ 
-                   x <<- y 
-                     # set function assigns the argument to x 
-                   I <<- NULL 
-                # Once the set function is called, Inverse is re-set to NULL (this is important if you redefine the matrix, x) 
-           } 
-           get <- function() x 
-           # get function returns the matrix 
-          
-       setInverse <- function(solve) I <<- solve 
-          # setInverse overrides the previous value of I and assigns the argument to Inverse (which is supposed to be the inverse of matrix x) 
-           
-        getInverse <- function() I 
-         # getInverse returns the Inverse 
-           
-           list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) 
-           # creates a list of the functions 
-           
-  } 
-
 
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
